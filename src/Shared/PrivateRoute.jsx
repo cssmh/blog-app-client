fceb4271be52dbdb89d9ae78e2dfa37de7ace682
Momen <touristmomen@gmail.com>
2025-02-03 22:15:00 +0600

    private route added

---
+++ src/Shared/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import useAuth from "../hooks/useAuth";
+import { Navigate, useLocation } from "react-router-dom";
+import Spinner from "../Component/Spinner/Spinner";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) return <Spinner size="88" />
+  if (user?.email) return children;
+
+  return <Navigate state={location?.pathname} to="/login"></Navigate>;
+};
+
+export default PrivateRoute;

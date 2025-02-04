c882e9ba9ae1fadb5e5479f0d40678b0dd34ab86
Momen <touristmomen@gmail.com>
2025-02-04 10:39:20 +0600

    search blogs with props

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,10 +1,18 @@
 import BlogHelmet from "./BlogHelmet";
+import { useState } from "react";
+import SearchBlog from "./SearchBlog";
 
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
   return (
     <>
       <div className="max-w-[1200px] 2xl:max-w-[85%] mx-auto">
         <BlogHelmet title="Home" />
+        <SearchBlog onSearch={handleSearch} />
       </div>
     </>
   );

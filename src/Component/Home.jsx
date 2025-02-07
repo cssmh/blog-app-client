8a66d62cf0108a3911b335ea93952c55cda1dddc
Momen <touristmomen@gmail.com>
2025-02-08 00:02:41 +0600

    LatestNews added

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -2,6 +2,7 @@ import BlogHelmet from "./BlogHelmet";
 import { useState } from "react";
 import SearchBlog from "./SearchBlog";
 import HomeBlogs from "../Pages/HomeBlogs";
+import LatestNews from "./LatestNews";
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -15,6 +16,7 @@ const Home = () => {
         <BlogHelmet title="Home" />
         <SearchBlog onSearch={handleSearch} />
         <HomeBlogs searchTerm={searchQuery} />
+        <LatestNews />
       </div>
     </>
   );
